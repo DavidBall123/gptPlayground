60b4e3e4092a53a02eb4239d31ab8eccf80f9264
David Ball <david.ball@a23.gov.au>
2023-06-26 10:23:04 +1000

    Add map branch middleware

--- ConsolePuzzles/MiddlewareTest/Program.cs
+++ ConsolePuzzles/MiddlewareTest/Program.cs
@@ -18,6 +18,21 @@ app.Use(async (context, next) =>
     await next.Invoke();
     Console.WriteLine($"Logic after executing the next delegate in the Use method");
 });
+app.Map("/usingmapbranch", builder =>
+{
+    builder.Use(async (context, next) =>
+    {
+        Console.WriteLine($"Map branch logic in the Use method before the next delegate");
+        await next.Invoke();
+        Console.WriteLine($"Map branch logic in the Use method after the next delegate");
+    });
+    builder.Run(async context =>
+    {
+        Console.WriteLine($"Writing the response to the client in the Run method");
+        await context.Response.WriteAsync("Hello from the map branch middleware component.");
+    });
+    
+});
 
 app.Run(async context =>
 {
