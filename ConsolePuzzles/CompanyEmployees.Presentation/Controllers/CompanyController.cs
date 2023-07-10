9ec2bc5c391c579be09f04ecf3977930512f2dab
David Ball <david.ball@a23.gov.au>
2023-07-10 11:02:57 +1000

    Add get all companies route

--- ConsolePuzzles/CompanyEmployees.Presentation/Controllers/CompanyController.cs
+++ ConsolePuzzles/CompanyEmployees.Presentation/Controllers/CompanyController.cs
@@ -1,9 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
 
 namespace CompanyEmployees.Presentation.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/companies")]
+    [ApiController]
     public class CompanyController: ControllerBase
     {
+        private readonly IServiceManager _serviceManager;
+
+        public CompanyController(IServiceManager serviceManager) => _serviceManager = serviceManager;
+
+        [HttpGet]
+        public IActionResult GetCompanies()
+        {
+            try
+            {
+                var companies = _serviceManager.CompanyService.GetAllCompanies(trackChanges: false);
+
+                return Ok(companies);
+            }
+            catch 
+            {
+                return StatusCode((int) HttpStatusCode.InternalServerError, "Internal server error");
+            }
+        }
     }
 }
--- ConsolePuzzles/Service.Contracts/ICompanyService.cs
+++ ConsolePuzzles/Service.Contracts/ICompanyService.cs
@@ -1,7 +1,10 @@
-﻿namespace Service.Contracts
+﻿using Entities.Models;
+
+namespace Service.Contracts
 {
     public interface ICompanyService
     {
+        IEnumerable<Company> GetAllCompanies(bool trackChanges);
 
     }
 }
