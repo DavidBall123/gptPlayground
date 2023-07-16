da4ba7937a758dac1d1871607fabe9713ff84792
David Ball <david.ball@a23.gov.au>
2023-07-17 09:09:01 +1000

    Added get all employees route

--- ConsolePuzzles/CompanyEmployees.Presentation/Controllers/Employees.Controller.cs
+++ ConsolePuzzles/CompanyEmployees.Presentation/Controllers/Employees.Controller.cs
@@ -10,5 +10,12 @@ namespace CompanyEmployees.Presentation.Controllers
         private readonly IServiceManager _service;
 
         public EmployeesController(IServiceManager service) => _service = service;
+
+        [HttpGet]
+        public IActionResult GetEmployeesForCompany(Guid companyId)
+        {
+            var employees = _service.EmployeeService.GetEmployees(companyId, trackChanges: false);
+            return Ok(employees);
+        }
     }
 }
